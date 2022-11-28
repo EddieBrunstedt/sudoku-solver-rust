752bc41c6c0d0dc308883cabe7bf7ccdf7229421
Eddie Brunstedt <eddie.brunstedt@protonmail.com>
2022-11-28 23:35:47 +0100

    Improve cli args

--- src/args.rs
+++ src/args.rs
@@ -1,13 +1,30 @@
-use clap::Parser;
+use clap::{ArgGroup, Parser};
+
+const AFTER: &'static str = "FORMAT OF INPUT:
+----------------
+Values 0-9 where 0 represents an empty cell.
+Any other character will be stripped away.
+
+Examples:
+530070000-600195000-098000060-800060003-400803001-700020006-060000280-000419005-000080079
+002810700/400200000/080009060/000600020/005040300/100007008/090000050/006000400/300000001
+
+Tip:
+If you use values from file (with '-f' flag), feel free to use line breaks
+in the file as they will be stripped away automatically.";
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[command(author, version, about = "Solve any Sudoku using a backtracking algorithm.", long_about = None, after_help = AFTER)]
+#[command(group(ArgGroup::new("input")
+                .required(true)
+                .args(["input_from_stdin", "input_from_file"]))
+)]
 pub struct SudokuArgs {
-    /// Each cell's value from left to right of the sudoku where 0 represents an empty cell
-    #[arg(short, long, value_name = "CELL_VALUES")]
-    pub sudoku_input: Option<String>,
+    /// Each cell's value from left to right of the Sudoku where 0 represents an empty cell
+    #[arg(short = 's', long, value_name = "values")]
+    pub input_from_stdin: Option<String>,
 
-    /// Solving your sudoku, no questions asked
-    #[arg(short, long)]
-    pub no_confirm: bool,
+    /// Read values from a file
+    #[arg(short = 'f', long, value_name = "file_path")]
+    pub input_from_file: Option<String>,
 }
